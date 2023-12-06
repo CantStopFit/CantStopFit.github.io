7b31110aebb31e332c8db11b2b8ce566fe620fc1
PulsePeak <151754354+PulsePeak@users.noreply.github.com>
2023-12-06 23:48:14 +0000

    workout item updates 18 days!

--- java/workoutA.js
+++ java/workoutA.js
@@ -1,119 +1,66 @@
-function startWorkoutA() {
-    document.getElementById('jumpRope').style.display = "block";
-    document.getElementById('startWorkoutA').style.display = "none";
-
+function situp() {
+    //Item Removal/Addition
+    document.getElementById('SitUpItem').style.display = "flex";
+    document.getElementById('jumpRopeItem').style.display = "none";
+    document.getElementById('bandedSideWalkItem').style.display = "none"
+    //Progress Bar
+    document.getElementById('jumpRopeProgress').classList.remove('active');
+    document.getElementById('jumpRopeProgress').classList.add('past');
+    document.getElementById('situpProgress').classList.add('active');
+    //Advancement Bar
+    document.getElementById('situpAdvancement').classList.add('active-btn');
+    document.getElementById('jumpRopeAdvancement').classList.remove('active-btn')
+    //Back Buttons
+    document.getElementById('situpBack').style.display = "block";
+    document.getElementById('fakeBack').style.display = "none"
+    //Forward Buttons
+    document.getElementById('jumpRopeContinue').style.display = "none";
+    document.getElementById('situpContinue').style.display = "block";
 }
+// function test(){
+      //   document.getElementById('jumpRopeProgress').classList.add('past');
+      // }
+      // function test2(){
+      //   document.getElementById('jumpRopeProgress').classList.remove('past');
+      //   document.getElementById('jumpRopeProgress').classList.add('active');
+      // }
+      //Jump Rope Timer
+      function startjumpRopeTimer() {
+        var duration = 2 * 60; // 2 minutes in seconds
+        var display = document.getElementById('jumpRopeTimer');
 
-function jumpRope() {
-    document.getElementById('jumpRope').style.display = "block";
-    document.getElementById('situp').style.display = "none";
-}
+        var jumpRopeTimer = duration, minutes, seconds;
+        setInterval(function () {
+          minutes = parseInt(jumpRopeTimer / 60, 10);
+          seconds = parseInt(jumpRopeTimer % 60, 10);
 
-function situps() {
-    document.getElementById('situp').style.display = "block";
-    document.getElementById('jumpRope').style.display = "none";
-    document.getElementById('bandedSideWalk').style.display = "none";
-}
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
 
-function bandedSideWalk() {
-    document.getElementById('bandedSideWalk').style.display = "block";
-    document.getElementById('situp').style.display = "none";
-    document.getElementById('gobletSquats').style.display = "none";
-}
+          display.textContent = minutes + ":" + seconds;
 
-function gobletSquats() {
-    document.getElementById('gobletSquats').style.display = "block";
-    document.getElementById('bandedSideWalk').style.display = "none";
-    document.getElementById('pushUps').style.display = "none";
-}
-
-function pushUps() {
-    document.getElementById('pushUps').style.display = "block";
-    document.getElementById('gobletSquats').style.display = "none";
-    document.getElementById('weightedCrossoverLunges').style.display = "none";
-}
-
-function weightedCrossoverLunges() {
-    document.getElementById('weightedCrossoverLunges').style.display = "block";
-    document.getElementById('pushUps').style.display = "none";
-    document.getElementById('pullUps').style.display = "none";
-}
-
-function pullUps() {
-    document.getElementById('pullUps').style.display = "block";
-    document.getElementById('weightedCrossoverLunges').style.display = "none";
-    document.getElementById('gluteBridges').style.display = "none";
-}
-
-function gluteBridges() {
-    document.getElementById('gluteBridges').style.display = "block";
-    document.getElementById('pullUps').style.display = "none";
-    document.getElementById('splitSquats').style.display = "none";
-}
-
-function splitSquats() {
-    document.getElementById('splitSquats').style.display = "block";
-    document.getElementById('gluteBridges').style.display = "none";
-    document.getElementById('plank').style.display = "none";
-}
-
-function plank() {
-    document.getElementById('plank').style.display = "block";
-    document.getElementById('splitSquats').style.display = "none";
-    document.getElementById('finishedWorkout').style.display = "none";
-}
-
-function finishedWorkout() {
-    document.getElementById('finishedWorkout').style.display = "block";
-    document.getElementById('plank').style.display = "none";
-}
-
-function finishedWorkoutCelebration() {
-    document.getElementById('complete-workout-header-id').style.display = "block";
-    document.getElementById('workout-finished').style.border = "1px solid rgb(60, 238, 0)";
-    document.getElementById('workout-finished').style.backgroundColor = "rgba(30, 255, 0, 0.59)";
-    document.getElementById('check-workout-finished').style.color = "rgb(0, 202, 27)";
-}
-
-
-// Jump Rope Timer
-function startjumpRopeTimer() {
-    var duration = 2 * 60; // 2 minutes in seconds
-    var display = document.getElementById('jumpRopeTimer');
-
-    var jumpRopeTimer = duration, minutes, seconds;
-    setInterval(function () {
-      minutes = parseInt(jumpRopeTimer / 60, 10);
-      seconds = parseInt(jumpRopeTimer % 60, 10);
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      display.textContent = minutes + ":" + seconds;
-
-      if (--jumpRopeTimer < 0) {
-        document.getElementById('jumpRopeTimer').innerHTML = '00:00';
+          if (--jumpRopeTimer < 0) {
+            document.getElementById('jumpRopeTimer').innerHTML = '00:00';
+          }
+        }, 1000);
       }
-    }, 1000);
-  }
+      //Plank Timer
+      function startPlankTimer() {
+        var duration = 2 * 60; // 2 minutes in seconds
+        var display = document.getElementById('plankTimer');
 
-// Plank Timer
-function startplankTimer() {
-    var duration = 2 * 60; // 2 minutes in seconds
-    var display = document.getElementById('plankTimer');
+        var plankTimer = duration, minutes, seconds;
+        setInterval(function () {
+          minutes = parseInt(plankTimer / 60, 10);
+          seconds = parseInt(plankTimer % 60, 10);
 
-    var plankTimer = duration, minutes, seconds;
-    setInterval(function () {
-      minutes = parseInt(plankTimer / 60, 10);
-      seconds = parseInt(plankTimer % 60, 10);
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+          display.textContent = minutes + ":" + seconds;
 
-      display.textContent = minutes + ":" + seconds;
-
-      if (--plankTimer < 0) {
-        document.getElementById('plankTimer').innerHTML = '00:00'
+          if (--plankTimer < 0) {
+            document.getElementById('plankTimer').innerHTML = '00:00';
+          }
+        }, 1000);
       }
-    }, 1000);
-  }
