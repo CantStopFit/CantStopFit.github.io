2397b7d50d185cb30d29716fd8fe570eb32bebff
Greyson Hubler <greysonhubler@gmail.com>
2023-12-27 09:04:31 -0600

    email subscribers form

---
+++ js/email-subs.js
@@ -0,0 +1,20 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyKttRpEu4ZtBy9oY-eYFi2o0opmditJoQHIhuBNsCpCNvmQsH4_6l31AiFaxW3OeN2/exec'
+        const form = document.forms['email-subscribers-form']
+        const popup = document.getElementById('email-subcribers-form');
+        const popupBackground = document.getElementById('email-subscribers-form-background');
+        var EmailSubForm = document.getElementById("EmailSubForm");
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+            EmailSubForm.reset();
+            popup.style.display = "none";
+            popupBackground.style.display = "none";
+        })
+
+function bye() {
+    document.getElementById('email-subscribers-form-background').style.display = 'none';
+    document.getElementById('email-subcribers-form').style.display = "none";
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    document.getElementById('email-subcribers-form').style.display = 'block';
+    document.getElementById('email-subscribers-form-background').style.display = 'block';
+  }, Math.floor(Math.random() * 20000));
